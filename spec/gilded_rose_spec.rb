d98c5c9623ae884e85d0f1325fd62a2a39751d7b
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-18 17:52:27 +0100

    Adds another test for backstage passes with less than 5 days to sell in

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -158,6 +158,15 @@ describe GildedRose do
         expect(items[0].quality).to eq 23
       end
 
+      it "Less than 5 days to sell in again" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 20)]
+        GildedRose.new(items).update_quality()
+
+        expect(items[0].name).to eq "Backstage passes to a TAFKAL80ETC concert"
+        expect(items[0].sell_in).to eq 0
+        expect(items[0].quality).to eq 23
+      end
+
     end
 
   end

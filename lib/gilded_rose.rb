eebd2814240451a0d5e16890229d0f81d004b954
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-18 19:55:57 +0100

    Refactored part of update_quality for backstage passes under 11 sell in days

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,19 +8,16 @@ class GildedRose
 
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros"
-
         item.quality = item.quality - 1 if item.quality > 0
-          
-
       else
+
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
+          if item.name == "Backstage passes to a TAFKAL80ETC concert" and item.sell_in < 11 and item.quality < 50
+
+            item.quality = item.quality + 1
+
+
             if item.sell_in < 6
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -28,10 +25,15 @@ class GildedRose
             end
           end
         end
+
       end
+
+
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
+
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -49,6 +51,8 @@ class GildedRose
           end
         end
       end
+
+
     end
   end
 end

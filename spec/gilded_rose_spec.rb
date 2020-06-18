a9546a5253a95dfa4534ddb5c56e59ad594ddaf6
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-18 14:51:16 +0100

    First test added for normal item, normal conditions

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,12 +3,19 @@ require 'gilded_rose'
 
 describe GildedRose do
 
-  # describe "#update_quality" do
-  #   it "does not change the name" do
-  #     items = [Item.new("foo", 0, 0)]
-  #     GildedRose.new(items).update_quality()
-  #     expect(items[0].name).to eq "fixme"
-  #   end
-  # end
+  describe "#update_quality" do
+    it "Normal item, normal conditions" do
+      items = [Item.new("Normal Item", 20, 20)]
+      GildedRose.new(items).update_quality()
+
+      expect(items[0].name).to eq "Normal Item"
+      expect(items[0].sell_in).to eq 19
+      expect(items[0].quality).to eq 19
+    end
+  end
 
 end
+
+# items = [Item.new("foo", 0, 0)]
+# GildedRose.new(items).update_quality()
+# expect(items[0].name).to eq "fixme"

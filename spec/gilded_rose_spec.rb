089e8b4d463ff0f5d2cfb871ef0595beb1301f29
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-18 14:54:28 +0100

    Test added for normal item, past sell in date

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,13 +4,25 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    it "Normal item, normal conditions" do
-      items = [Item.new("Normal Item", 20, 20)]
-      GildedRose.new(items).update_quality()
+    describe "Normal item" do
 
-      expect(items[0].name).to eq "Normal Item"
-      expect(items[0].sell_in).to eq 19
-      expect(items[0].quality).to eq 19
+      it "Normal conditions" do
+        items = [Item.new("Normal Item", 20, 20)]
+        GildedRose.new(items).update_quality()
+
+        expect(items[0].name).to eq "Normal Item"
+        expect(items[0].sell_in).to eq 19
+        expect(items[0].quality).to eq 19
+      end
+
+      it "Past sell in date" do
+        items = [Item.new("Normal Item", -1, 20)]
+        GildedRose.new(items).update_quality()
+
+        expect(items[0].name).to eq "Normal Item"
+        expect(items[0].sell_in).to eq -2
+        expect(items[0].quality).to eq 18
+      end
     end
   end
 

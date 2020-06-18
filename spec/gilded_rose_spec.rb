7c2c438b5ce4e4e56a16ac52614883eedb26f38a
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-18 17:22:42 +0100

    Adds test for aged brie in normal conditions

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,11 +1,10 @@
-# require File.join(File.dirname(__FILE__), 'gilded_rose')
 require 'gilded_rose'
 
 describe GildedRose do
 
   describe "#update_quality" do
-    describe "Normal item" do
 
+    describe "Normal item" do
       it "Normal conditions" do
         items = [Item.new("Normal Item", 20, 20)]
         GildedRose.new(items).update_quality()
@@ -51,10 +50,18 @@ describe GildedRose do
         expect(items[0].quality).to eq 0
       end
     end
+
+
+    describe "Aged Brie" do
+      it "Normal conditions" do
+        items = [Item.new("Aged Brie", 20, 20)]
+        GildedRose.new(items).update_quality()
+
+        expect(items[0].name).to eq "Aged Brie"
+        expect(items[0].sell_in).to eq 19
+        expect(items[0].quality).to eq 21
+      end
+    end
   end
 
 end
-
-# items = [Item.new("foo", 0, 0)]
-# GildedRose.new(items).update_quality()
-# expect(items[0].name).to eq "fixme"

7c79a57251e1e521cf981a9587c793fcb396b273
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-18 17:16:47 +0100

    Adds test for normal item at lowest quality

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -32,6 +32,15 @@ describe GildedRose do
         expect(items[0].sell_in).to eq -3
         expect(items[0].quality).to eq 16
       end
+
+      it "At lowest quality" do
+        items = [Item.new("Normal Item", 20, 0)]
+        GildedRose.new(items).update_quality()
+
+        expect(items[0].name).to eq "Normal Item"
+        expect(items[0].sell_in).to eq 19
+        expect(items[0].quality).to eq 0
+      end
     end
   end
 

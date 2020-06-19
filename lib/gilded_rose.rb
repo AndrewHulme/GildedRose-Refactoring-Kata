83f89a6d9d5e7fa19bdf6aee0563686adab6783f
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-19 15:53:22 +0100

    Moves methods to private and refactors update_quality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,35 +8,18 @@ class GildedRose
     @conjured = "Conjured Mana Cake"
   end
 
-  def catch_quality_outliers(item)
-
-    if item.quality > 50 and item.quality < 80
-      item.quality = 50
-    elsif item.quality < 0
-      item.quality = 0
-    end
-
-  end
-
-  def update_sell_in(item)
-    item.sell_in -= 1 if item.name != @sulfuras
-  end
-
-
   def update_quality()
 
     @items.each do |item|
-      if item.name != @agedbrie and item.name != @backstagepass and item.name != @sulfuras and item.name != @conjured and item.quality > 0
+      if item_normal(item) and item.quality > 0
         item.sell_in <= 0 ? item.quality -= 2 : item.quality -= 1
-      elsif item.name == @conjured and item.sell_in <= 0
-        item.quality -= 4
-      elsif item.name == @conjured
-        item.quality -= 2
-      elsif item.name == @backstagepass and item.sell_in <= 0
+      elsif item_conjured(item)
+        item.sell_in <= 0 ? item.quality -= 4 : item.quality -= 2
+      elsif item_backstage_pass(item) and item.sell_in <= 0
         item.quality = 0
-      elsif item.name == @backstagepass and item.sell_in < 6
+      elsif item_backstage_pass(item) and item.sell_in < 6
         item.quality += 3
-      elsif item.name == @backstagepass and item.sell_in < 11
+      elsif item_backstage_pass(item) and item.sell_in < 11
         item.quality += 2
       elsif item.name != @sulfuras and item.quality > 0
         item.sell_in <= 0 ? item.quality += 2 : item.quality += 1
@@ -48,6 +31,34 @@ class GildedRose
     end
 
   end
+
+
+  private
+
+  def catch_quality_outliers(item)
+    if item.quality > 50 and item.quality < 80
+      item.quality = 50
+    elsif item.quality < 0
+      item.quality = 0
+    end
+  end
+
+  def update_sell_in(item)
+    item.sell_in -= 1 if item.name != @sulfuras
+  end
+
+  def item_normal(item)
+    item.name != @agedbrie and item.name != @backstagepass and item.name != @sulfuras and item.name != @conjured
+  end
+
+  def item_conjured(item)
+    item.name == @conjured
+  end
+
+  def item_backstage_pass(item)
+    item.name == @backstagepass
+  end
+
 end
 
 
